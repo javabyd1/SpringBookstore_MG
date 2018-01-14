f46763c4b64999dd4a6f47f67ccb03b67e9b691e
Hart Ungalled <przegiegaknieje@gmail.com>
2018-01-14 15:47:09 +0100

    Add experimental frontend to read rest ending
    Server.js need to be run via node.js, html page is visable on port 9999

--- src/main/java/com/sda/springstarter/demo/controller/BookRestController.java
+++ src/main/java/com/sda/springstarter/demo/controller/BookRestController.java
@@ -5,6 +5,7 @@ import com.sda.springstarter.demo.service.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,11 +23,13 @@ public class BookRestController {
     @Autowired
     private BookServiceImpl bookService;
 
+    @CrossOrigin(value = "http://localhost:9999")
     @GetMapping
     public List<Book> getAll(){
         return bookService.getAll();
     }
 
+    @CrossOrigin(value = "http://localhost:9999")
     @PostMapping("/addonebook")
     public void save(@RequestBody Book book){
         bookService.save(book);
